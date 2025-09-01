40b298a40f23f51b2d67320a6560a338bdde322a
chandu_116 <118456799+chandra71@users.noreply.github.com>
2025-09-01 23:05:49 +0530

    Add files via upload

---
+++ New portfolio/script.js
@@ -0,0 +1,7 @@
+// Mobile Nav Toggle
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
